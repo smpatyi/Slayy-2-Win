5bfb3418d4e0b7a20f6ef8806719f01bf2e82c12
Sara Patyi <67295329+smpatyi@users.noreply.github.com>
2024-01-27 14:42:06 -0500

    adding in success sprites; no functionality yet

--- Cucumber/Assets/Scenes/Stage-Scripts/NoteObject.cs
+++ Cucumber/Assets/Scenes/Stage-Scripts/NoteObject.cs
@@ -8,6 +8,11 @@ public class NoteObject : MonoBehaviour
 
     public KeyCode keyToPress;
 
+    public GameObject downVictory;
+    public GameObject leftVictory;
+    public GameObject rightVictory;
+    public GameObject upVictory;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,18 +26,37 @@ public class NoteObject : MonoBehaviour
             if(canBePressed) {
                 gameObject.SetActive(false);
             }
+
         }
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
         if(other.tag == "Activator") {
             canBePressed = true;
+            // if(Input.GetKeyDown(keyToPress.DownArrow)){
+            //     downVictory.SetActive(true);
+            // } else if(Input.GetKeyDown(keyToPress.UpArrow)){
+            //     upVictory.SetActive(true);
+            // } else if(Input.GetKeyDown(keyToPress.LeftArrow)){
+            //     leftVictory.SetActive(true);
+            // } else if(Input.GetKeyDown(keyToPress.RightArrow)){
+            //     rightVictory.SetActive(true);
+            // }
         }
     }
 
     private void OnTriggerExit2D(Collider2D other) {
         if(other.tag == "Activator") {
             canBePressed = false;
+            // if(Input.GetKeyUp(keyToPress.DownArrow)){
+            //     downVictory.SetActive(false);
+            // } else if(Input.GetKeyUp(keyToPress.UpArrow)){
+            //     upVictory.SetActive(false);
+            // } else if(Input.GetKeyUp(keyToPress.LeftArrow)){
+            //     leftVictory.SetActive(false);
+            // } else if(Input.GetKeyUp(keyToPress.RightArrow)){
+            //     rightVictory.SetActive(false);
+            // }
         }
     }
 }
