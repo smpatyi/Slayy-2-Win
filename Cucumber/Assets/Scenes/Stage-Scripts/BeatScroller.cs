aa6355d4909bcdfcef29a612260362cd093d113a
Sara Patyi <67295329+smpatyi@users.noreply.github.com>
2024-01-28 10:12:56 -0500

    gone back to prior green arrow continual movement success condition

--- Cucumber/Assets/Scenes/Stage-Scripts/BeatScroller.cs
+++ Cucumber/Assets/Scenes/Stage-Scripts/BeatScroller.cs
@@ -7,7 +7,7 @@ public class BeatScroller : MonoBehaviour
 
     public float beatTempo;
 
-    public bool hasStarted;
+    // public bool hasStarted;
 
 
     // Start is called before the first frame update
@@ -19,13 +19,15 @@ public class BeatScroller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(!hasStarted) {
-            if(Input.anyKeyDown) {
-                hasStarted = true;
-            }
-        } else {
-            transform.position += new Vector3(0f, beatTempo * Time.deltaTime, 0f);
-        }
+        // if(!hasStarted) {
+        //     if(Input.anyKeyDown) {
+        //         hasStarted = true;
+        //     }
+        // } else {
+        //     transform.position += new Vector3(0f, beatTempo * Time.deltaTime, 0f);
+        // }
+
+        transform.position += new Vector3(0f, beatTempo * Time.deltaTime, 0f);
         
     }
 }
